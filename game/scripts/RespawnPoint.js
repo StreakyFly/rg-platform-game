ff539b40ffc8e706825b59e421a5869a4266410b
StreakyFly <53691430+StreakyFly@users.noreply.github.com>
2024-01-14 12:28:11 +0100

    Delete game/scripts/RespawnPoint.js

--- game/scripts/RespawnPoint.js
+++
@@ -1,8 +0,0 @@
-export class RespawnPoint {
-    constructor(checkPointTransform, yaw) {
-        this.translation = checkPointTransform.translation;
-        this.rotation = checkPointTransform.rotation;
-        this.yaw = yaw;
-        this.pitch = 0;
-    }
-}
